62a5fee7a9844614596353d3b4e9a5d5052d82e4
CoyAri <mamonjeyms@gmail.com>
2024-02-23 18:16:21 -0500

    match done except reset (initial hiding doesnt work on reset)

--- src/match/Card.js
+++ src/match/Card.js
@@ -1,12 +1,11 @@
 
-export default function Card({value, handleCardClick, flip, match}){
-    return(
+export default function Card({ value, handleCardClick, flip, match }) {
+    return (
         <>
-            <div 
-                className={`${flip ? "bg-stone-400": "bg-stone-300 hover:bg-stone-200 active:bg-stone-400" }  size-[5rem] flex justify-center button rounded transition ease-in-out duration-500`} onClick={handleCardClick}>
-                <button className="text-black size-full text-2xl" disabled={flip}>{flip ? value : ""}</button>
+            <div
+            className={`${match ? "bg-stone-600" : flip ? "bg-stone-400" : "bg-stone-300 hover:bg-stone-200 active:bg-stone-400"}  size-[5rem] flex justify-center button rounded transition ease-in-out duration-500`} onClick={handleCardClick}>
+                <button className="text-black size-full text-2xl" disabled={flip || match}>{flip || match ? value : ""}</button>
             </div>
-            {match && <div>hello there</div>}
         </>
     )
 }
--- src/match/MatchBoard.js
+++ src/match/MatchBoard.js
@@ -5,14 +5,18 @@ export default function MatchBoard() {
 
     const [cards, setCards] = useState([])
     const [showCards, setShowCards] = useState(Array(16).fill(true))
-    const [matched, setMatched] = useState([])
+    const [matched, setMatched] = useState(Array(16).fill(false))
     const [card, setCard] = useState(null)
+    const [countMoves, setCountMoves] = useState(0)
+    const [reset, setReset] = useState(false)
+    const [start, setStart] = useState(false)
+    let status
 
     useEffect(() => {
         const symbols = ["❤️", "🎻", "🌸", "🚴", "👑", "🃏", "🎲", "🎱"]
         const deck = [...symbols, ...symbols].sort(() => 0.5 - Math.random())
         setCards(deck)
-    }, [])
+    }, [start], [])
 
     useEffect(() => {
         const hideCards = Array(16).fill(false)
@@ -20,32 +24,74 @@ export default function MatchBoard() {
         const hideTime = setTimeout(() => {
             setShowCards(hideCards)
         }, 2000)
-    }, [])
+    }, [start], [])
+
+
 
     const handleClick = (index) => {
-        {/** have a counter to check/keep track to how many matched cards. also if we can keep track of how many moves/score the player has */ }
-
-        if (card !== null) {
-            if (cards[index] === cards[card]) {
-                {/* records true to index and card(flipped card) in matched */ }
-
-            }
-            {/** put here what happens if card don't match. setTimeout(), 200 to have a bit of delay till cards are flipped back to null/empty */ }
-
+        {/** to make it so cards don't get flipped mid check/match */}
+        if (showCards.filter(Boolean).length === 2) {
+            return
         }
+
         else {
-            {/** this should only happen if there is no flipped card */ }
             const flipCard = showCards.slice()
             flipCard[index] = true
             setShowCards(flipCard)
+        }
+
+        if (card === null) {
             setCard(index)
         }
+
+        else if (card !== null && cards[index] === cards[card]) {
+            {/* records true to index and card(flipped card) in matched */ }
+            const matchedCards = matched.slice()
+            matchedCards[index] = true
+            matchedCards[card] = true
+            setMatched(matchedCards)
+            const hideCards = Array(16).fill(false)
+            setShowCards(hideCards)
+            setCard(null)
+            setCountMoves(countMoves + 1)
+            {/**reset button not showing up for some reason (it works now :>) */}
+            if (!matchedCards.includes(false)) {
+                console.log('Resetting?')
+                setReset(true)
+            }
+        }
+        else {
+            {/** this should only happen if the cards don't match */ }
+            setCountMoves(countMoves + 1)
+            const hideCards = Array(16).fill(false)
+            setCard(null)
+            const hideTime = setTimeout(() => {
+                setShowCards(hideCards)
+            }, 600)
+        }
+
+    }
+
+    const handleReset = () => {
+        console.log('Indeed!')
+        const resetCards = Array(16).fill(false)
+        setCards(resetCards)
+        setMatched(resetCards)
+        setCountMoves(0)
+        setStart(!start)
+    }
+
+    if (!matched.includes(false)) {
+        status = "Total moves : "
+    }
+    else {
+        status = "Num of moves : "
     }
 
     return (
         <>
             <div className="grid grid-rows-5">
-                <div></div>
+                <div className="text-center m-auto">{status + countMoves}</div>
                 <div className="grid grid-cols-4 place-content-center gap-4 row-span-3">
                     <Card value={cards[0]} handleCardClick={() => handleClick(0)} flip={showCards[0]} match={matched[0]} />
                     <Card value={cards[1]} handleCardClick={() => handleClick(1)} flip={showCards[1]} match={matched[1]} />
@@ -64,8 +110,9 @@ export default function MatchBoard() {
                     <Card value={cards[14]} handleCardClick={() => handleClick(14)} flip={showCards[14]} match={matched[14]} />
                     <Card value={cards[15]} handleCardClick={() => handleClick(15)} flip={showCards[15]} match={matched[15]} />
                 </div>
-                <div></div>
-
+                <div className="m-auto">
+                    <button className={`${reset ? 'block' : 'hidden'} text-center size-12 text-2xl`} onClick={handleReset}>🎅🏻</button>
+                </div>
             </div>
         </>
     )
